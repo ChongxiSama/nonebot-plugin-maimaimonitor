ac71253d4a08d7d24344941dce97cf13e227501f
ChongxiSama <chongxiace666@outlook.com>
2025-12-26 00:00:52 +0800

    fix1200

--- nonebot_plugin_maimaimonitor/config.py
+++ nonebot_plugin_maimaimonitor/config.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 class Config(BaseSettings):
+    model_config = ConfigDict(extra='ignore')
     maimai_bot_client_id: str
     maimai_bot_private_key: str
     maimai_bot_display_name: str
